6bb364cdf23b028a9882e54b9edcf51ff50c5bb5
Seda <sedakant@gmail.com>
2023-06-06 22:39:39 -0400

    practice lwc

---
+++ force-app/main/default/lwc/practice27/practice27.js
@@ -0,0 +1,27 @@
+import { LightningElement, api, wire } from "lwc";
+import CASE_NUMBER_FIELD from "@salesforce/schema/Case.CaseNumber";
+import STATUS_FIELD from "@salesforce/schema/Case.Status";
+import { getFieldValue, getRecord } from "lightning/uiRecordApi";
+
+// this component is meant to only be used in case record page 
+// or anywhere the recordId can be passed from outside
+export default class Practice27 extends LightningElement {
+	// we will get recordId from record page directly 
+    @api recordId;
+    // based on recordId get the the entire case object and whatever fields you need 
+    @wire(getRecord, {
+        recordId: '$recordId',
+        fields: [CASE_NUMBER_FIELD, STATUS_FIELD]
+    })
+    caseRecord; 
+
+    get caseNumber() {
+        return getFieldValue(this.caseRecord.data, CASE_NUMBER_FIELD);
+    }
+
+    get caseStatus() {
+        return getFieldValue(this.caseRecord.data, STATUS_FIELD);
+    }
+    
+
+}
---
+++ force-app/main/default/lwc/practice30Child/practice30Child.js
@@ -0,0 +1,13 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Practice30Child extends LightningElement {
+
+    //message = 'Hello World';
+
+    @api // make this property public , so parent can provide the value 
+    message; 
+
+    @api
+    num; 
+
+}
